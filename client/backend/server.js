9ca9f22ba92981576492be2b8bc96e94e90e2f91
preethamgande <preethamgande.engineer@gmail.com>
2025-09-04 17:18:21 +0530

    made changes to server origin

--- client/backend/server.js
+++ client/backend/server.js
@@ -94,6 +94,11 @@ const storage = multer.diskStorage({
 
 const upload = multer({ storage });
 
+app.use((req, res, next) => {
+  console.log("Incoming request:", req.method, req.url);
+  next();
+});
+
 // making sure the folder is publicly accessible..
 app.use("/uploads", express.static(uploadDir));
 
