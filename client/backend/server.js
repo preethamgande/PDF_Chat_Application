c28fc63257899b3eb8dc6a42950a100b403897fc
preethamgande <preethamgande.engineer@gmail.com>
2025-09-04 11:36:53 +0530

    made corrections for cors

--- client/backend/server.js
+++ client/backend/server.js
@@ -49,8 +49,16 @@ const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
 // app.use(express.json({ limit: "10mb" }));
 // app.use(express.urlencoded({ extended: true, limit: "10mb" }));
 
-app.use(cors());
-app.use(express.json());
+app.use(
+  cors({
+    origin: [
+      "https://pdf-chat-application-frontend.onrender.com", // your deployed frontend
+      "http://localhost:5173", // local dev
+    ],
+    methods: ["GET", "POST", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
 
 const uploadDir = "uploads";
 if (!fs.existsSync(uploadDir)) {
